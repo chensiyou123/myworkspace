bb1e74de63f876def97b9cc6eda4a4e04f20dfcc
chensiyou <905630790@qq.com.cn>
2019-04-18 23:37:58 +0800

    配置代理

---
+++ vue-blog/blogServer/src/main/java/com.csy.blogServer/controller/LoginRegController.java
@@ -0,0 +1,17 @@
+package com.csy.blogServer.controller;
+
+import com.csy.blogServer.service.UserService;
+import com.csy.blogServer.utils.RespBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginRegController {
+    @Autowired
+    UserService userService;
+    @RequestMapping("/login")
+    public RespBean loginError() {
+        return new RespBean("error", "登录失败!");
+    }
+}
