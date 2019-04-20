27503ae3f859bc1b775be9966af97a6f955da8da
chensiyou <905630790@qq.com.cn>
2019-04-20 12:22:28 +0800

    加密1

---
+++ springboot2-springSecurity02/src/main/java/com/cay/springsecurity01/config/WebSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.cay.springsecurity01.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        /*
+         * 配置为从内存中进行加载认证信息.
+         * 这里配置了两个用户 admin和user
+         */
+        auth.inMemoryAuthentication()
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("admin")
+                .password(new BCryptPasswordEncoder().encode("123456"))
+                .roles();
+        auth.inMemoryAuthentication()
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("user")
+                .password(new BCryptPasswordEncoder().encode("123456"))
+                .roles();
+    }
+}
