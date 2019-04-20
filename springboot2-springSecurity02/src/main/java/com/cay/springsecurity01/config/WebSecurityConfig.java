cf83f398f3fb7b31d0451dedc2d8f64e295f9b55
chensiyou <905630790@qq.com.cn>
2019-04-20 22:51:25 +0800

    自定义AccssDesionManager

--- springboot2-springSecurity02/src/main/java/com/cay/springsecurity01/config/WebSecurityConfig.java
+++ springboot2-springSecurity02/src/main/java/com/cay/springsecurity01/config/WebSecurityConfig.java
@@ -1,10 +1,9 @@
 package com.cay.springsecurity01.config;
 
-import com.cay.springsecurity01.filter.AfterLoginFilter;
-import com.cay.springsecurity01.filter.AtLoginFilter;
-import com.cay.springsecurity01.filter.BeforeLoginFilter;
+import com.cay.springsecurity01.filter.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +11,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -37,17 +38,36 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
+    @Bean
+    public FilterInvocationSecurityMetadataSource filterInvocationSecurityMetadataSource() {
+        return new MyFilterInvocationSecurityMetadataSource();
+    }
+    @Bean
+    public MyAccessDecisionManager accessDecisionManager() {
+        return new MyAccessDecisionManager();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/login").permitAll()
+                .antMatchers("/login").permitAll()// 设置所有人都可以访问登录页面
                 .antMatchers("/","/index").permitAll()
-                .anyRequest().access("@authService.canAccess(request,authentication)")
+                .antMatchers("/test/**","/test1/**").permitAll()
+                .antMatchers("/res/**/*.{js,html}").permitAll()
+                .withObjectPostProcessor(new MyObjectPostProcessor())
                 .anyRequest().authenticated()  // 任何请求,登录后可以访问
                 .and()
                 .formLogin().loginPage("/login");
 
     }
+    private class MyObjectPostProcessor implements ObjectPostProcessor<FilterSecurityInterceptor> {
+        @Override
+        public <O extends FilterSecurityInterceptor> O postProcess(O fsi) {
+            fsi.setSecurityMetadataSource(filterInvocationSecurityMetadataSource());
+            fsi.setAccessDecisionManager(accessDecisionManager());
+            return fsi;
+        }
+
+    }
+
 }
---
+++ springboot2-springSecurity02/src/main/java/com/cay/springsecurity01/filter/MyFilterInvocationSecurityMetadataSource.java
@@ -0,0 +1,70 @@
+package com.cay.springsecurity01.filter;
+
+import com.cay.springsecurity01.service.PermissionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.access.SecurityConfig;
+import org.springframework.security.web.FilterInvocation;
+import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+public class MyFilterInvocationSecurityMetadataSource implements FilterInvocationSecurityMetadataSource {
+
+    @Autowired
+    private PermissionService permissionService;
+    @Override
+    public Collection<ConfigAttribute> getAttributes(Object object) throws IllegalArgumentException {
+        System.out.println("MyFilterInvocationSecurityMetadataSource.getAttributes()");
+        java.util.Map<String, Collection<ConfigAttribute>> map = permissionService.getPermissionMap();
+        FilterInvocation filterInvocation = (FilterInvocation) object;
+        System.out.println(filterInvocation.getFullRequestUrl());
+        if (isMatcherAllowedRequest(filterInvocation)) return null ; //return null 表示允许访问，不做拦截
+        HttpServletRequest request = filterInvocation.getHttpRequest();
+        String resUrl; //URL规则匹配.
+        AntPathRequestMatcher matcher;
+        for(Iterator<String> it  = map.keySet().iterator();it.hasNext();) {
+            resUrl = it.next();
+            matcher = new AntPathRequestMatcher(resUrl);
+            if(matcher.matches(request)) {
+                System.out.println(map.get(resUrl));
+                return map.get(resUrl);
+            }
+        }
+        //SecurityConfig.createList("ROLE_USER");
+        //方式一：没有匹配到,直接是白名单了.不登录也是可以访问的。
+        //return null;
+        //方式二：配有匹配到，需要指定相应的角色：
+        return SecurityConfig.createList("ROLE_admin");
+    }
+    /**
+     * 判断当前请求是否在允许请求的范围内
+     * @param fi 当前请求
+     * @return 是否在范围中
+     */
+    private boolean isMatcherAllowedRequest(FilterInvocation fi){
+        return allowedRequest().stream().map(AntPathRequestMatcher::new)
+                .filter(requestMatcher -> requestMatcher.matches(fi.getHttpRequest()))
+                .toArray().length > 0;
+    }
+    /**
+     * @return 定义允许请求的列表
+     */
+    private List<String> allowedRequest(){
+        return Arrays.asList("/login","/css/**","/fonts/**","/js/**","/scss/**","/img/**");
+    }
+    @Override
+    public Collection<ConfigAttribute> getAllConfigAttributes() {
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return true;
+    }
+}
