4e1e0a0a5655975d154c633e59de1010e26cf3d4
chensiyou <905630790@qq.com.cn>
2019-04-20 14:37:16 +0800

    退出系统、主页面

--- springboot2-springSecurity02/src/main/java/com/cay/springsecurity01/controller/HomeController.java
+++ springboot2-springSecurity02/src/main/java/com/cay/springsecurity01/controller/HomeController.java
@@ -5,9 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
     @GetMapping("/login")
     public String login() {
         return "/login";
+    }
+
+    @GetMapping({"","/","/index"})
+    public String index() {
+        return "/index";
     }
 
 }
